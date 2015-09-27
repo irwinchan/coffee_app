4ae2848e67cf2493f5c194a696cae6ae75c3d52b
Irwin <irwinchan@me.com>
2015-09-27 13:23:50 -0400

    Add bcrypt when creating a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150927150231) do
+ActiveRecord::Schema.define(version: 20150927171031) do
 
   create_table "recipes", force: :cascade do |t|
     t.integer  "coffee_id"
@@ -33,8 +33,9 @@ ActiveRecord::Schema.define(version: 20150927150231) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
